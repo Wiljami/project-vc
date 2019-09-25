7e3fc2d2ccfbfe9446aac63248d47a19e037eecd
Viljami Pietarila <viljami.pietarila@gmail.com>
2019-09-25 12:33:45 +0300

    App.java: Add parseArgs() function.
    Add parseArgs method that will take the arguments passed to the
    program from the command line.

--- src/main/java/fi/tuni/tiko/App.java
+++ src/main/java/fi/tuni/tiko/App.java
@@ -6,9 +6,17 @@ package fi.tuni.tiko;
  */
 public class App 
 {
+    static Calculator calculator;
     public static void main( String[] args )
     {
-        Calculator calculator = new Calculator();
+        calculator = new Calculator();
+        if (args.length > 0) {
+            parseArgs(args);
+        }
+    }
+
+    private static void parseArgs(String[] args) {
+
     }
 
     public int addition(int x, int y) {
