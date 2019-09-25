7319acc62e50594450efe9a277128014cb567b7e
Maija <testi@testi.com>
2019-09-25 21:50:04 +0300

    Add one test calculator to AppTest class and remove redundant ones.

--- src/test/java/fi/tuni/tiko/AppTest.java
+++ src/test/java/fi/tuni/tiko/AppTest.java
@@ -6,24 +6,22 @@ import fi.tuni.tiko.Calculator;
 
 public class AppTest 
 {
+    Calculator testCalc = new Calculator();
 
     @Test
     public void subtractionTest(){
-	Calculator testCalc = new Calculator();
        	Assert.assertEquals(testCalc.subtract(2, 1), 1);
 	Assert.assertEquals(testCalc.subtract(-5, 5), -10);
     }
 
     @Test
     public void additionTest() {
-	Calculator testCalc = new Calculator();
 	Assert.assertEquals(testCalc.addition(2,1),3);
 	Assert.assertEquals(testCalc.addition(-5,5),0);
     }
 
     @Test
     public void multiplicationTest() {
-        Calculator testCalc = new Calculator();
         Assert.assertEquals(testCalc.multiplication(5,5),25);
         Assert.assertEquals(testCalc.multiplication(-5,5),-25);
     }
