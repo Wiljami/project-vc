5400c310a8d9ccef071891e2bfad8d5f94e43ce3
Tiina Malinen <malitsu@gmail.com>
2019-09-26 14:40:19 +0300

    Start building cardlayout.

--- src/main/java/fi/tuni/tiko/Window.java
+++ src/main/java/fi/tuni/tiko/Window.java
@@ -1,5 +1,6 @@
 package fi.tuni.tiko;
 import javax.swing.*;
+import javax.swing.border.*;
 import java.awt.*;
 
 class Window extends JFrame {
@@ -7,10 +8,17 @@ class Window extends JFrame {
     JButton calcButton;
     Container container;
     JTextArea inputField;
+    JPanel panel;
+    CardLayout cardLayout;
 
     public Window(String title) {
         super(title);
         setSize(600, 600);
+
+        cardLayout = new CardLayout();
+        panel = new JPanel(cardLayout);
+        container = getContentPane();
+
         setVisible(true);
     }
 
