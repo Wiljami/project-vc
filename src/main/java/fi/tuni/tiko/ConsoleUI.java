72cef58a74ec87b96405715f8fc085fa54dedb96
Viljami Pietarila <viljami.pietarila@gmail.com>
2019-09-26 15:22:50 +0300

    ConsoleUI.java: Add very basic commands.
    Add very basic commands and functionality to the ConsoleUI class.
    Currently adds quit and help commands.

--- src/main/java/fi/tuni/tiko/ConsoleUI.java
+++ src/main/java/fi/tuni/tiko/ConsoleUI.java
@@ -1,17 +1,32 @@
 package fi.tuni.tiko;
 
+import java.util.Scanner;
+
 /**
  * An UI to be used in console.
  */
 
 class ConsoleUI {
     private Calculator calculator;
+    private Scanner input;
 
     public ConsoleUI(Calculator calculator) {
         this.calculator = calculator;
+        this.input = new Scanner(System.in);
     }
 
     public void start() {
-        
+        System.out.println("Welcome to the calculator.");
+        System.out.println("Type help for commands.");
+
+        while (true) {
+            String command = input.nextLine();
+            if (command.equals("help")) {
+                System.out.println("quit to quit");
+            } else if (command.equals("quit")) {
+                System.out.println("Good bye");
+                break;
+            }
+        }
     }
 }
