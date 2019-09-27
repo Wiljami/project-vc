af2b12ef6685598e4d09b51f6515e9592de75c13
Hanna Tuominen <hanna.h.tuominen@tuni.fi>
2019-09-27 19:37:49 +0300

    SecretSnake: Add attributes for board, its length.
    Add char array board for the gameboard,
    int lengthone for the rows,
    int lengthtwo for the cols.

--- src/main/java/fi/tuni/tiko/SecretSnake.java
+++ src/main/java/fi/tuni/tiko/SecretSnake.java
@@ -1,19 +1,21 @@
 package fi.tuni.tiko;
 
-import java.util.ArrayList;
-
 public class SecretSnake {
-    
+    char[][] board = new char[20][20];
+    int lengthone = board.length;
+    int lengthtwo = board[0].length;
+
     public SecretSnake() {
         
     }
 
     public void startSnake() {
-
+        createBoard();
+        printBoard();
     }
 
     public void createBoard() {
-        
+    
     }
 
     public void printBoard() {
