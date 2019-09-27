3e1b16cee15baecf627e9ea1c7fe9225dfb388c7
Maija <testi@testi.com>
2019-09-27 21:23:05 +0300

    Include user input in poemGenerator.
    User can choose between 6 poets.

--- src/main/java/fi/tuni/tiko/SecretPoemGenerator.java
+++ src/main/java/fi/tuni/tiko/SecretPoemGenerator.java
@@ -19,6 +19,10 @@ public class SecretPoemGenerator{
         System.out.println("Eino Leino    William Shakespeare    Thomas Tranströmer    Charles Baudelaire    Edith Södergran");
         System.out.println("Pick a poet and press enter: ");
         String poet1 = scanner.nextLine();
+        System.out.println("Pick another poet: ");
+        String poet2 = scanner.nextLine();
+        System.out.println();
+        System.out.println(poet1 + " + " + poet2 + ". You got it! [Press Enter]" );
     }
 
 }
