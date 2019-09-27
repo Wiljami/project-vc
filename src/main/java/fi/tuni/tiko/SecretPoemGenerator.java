8f6afb163e593731f26ed321e4f4acac22e6976d
Maija <testi@testi.com>
2019-09-27 21:05:09 +0300

    Poem generator: Add askPoet() method.

--- src/main/java/fi/tuni/tiko/SecretPoemGenerator.java
+++ src/main/java/fi/tuni/tiko/SecretPoemGenerator.java
@@ -1,13 +1,24 @@
 package fi.tuni.tiko;
+import java.util.Scanner;
 
 public class SecretPoemGenerator{
+    Scanner scanner;
+
     public SecretPoemGenerator(){
-        
+        scanner = new Scanner(System.in);
     }
 
     public void startPoemGenerator(){
         System.out.println("You found a Secret Poem Generator.");
         System.out.println("Welcome, Friend!");
+        System.out.println("         **************************************");
+        System.out.println();
+    }
+
+    public void askPoets(){
+        System.out.println("Eino Leino    William Shakespeare    Thomas Tranströmer    Charles Baudelaire    Edith Södergran");
+        System.out.println("Pick a poet and press enter: ");
+        String poet1 = scanner.nextLine();
     }
 
 }
