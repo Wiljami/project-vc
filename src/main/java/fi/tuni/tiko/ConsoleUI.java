4f8a66d3c7b1f3599034acf7fbf13f54d11b1db8
Viljami Pietarila <viljami.pietarila@gmail.com>
2019-09-29 10:39:32 +0300

    ConsoleUI: Add division keyword
    Add keyword for DIVISION and several Strings to work as commands for it.

--- src/main/java/fi/tuni/tiko/ConsoleUI.java
+++ src/main/java/fi/tuni/tiko/ConsoleUI.java
@@ -14,6 +14,7 @@ class ConsoleUI {
     private final String[] ADDITION = {"addition", "add", "+"};
     private final String[] SUBSTRACTION = {"substraction", "sub", "-"};
     private final String[] MULTIPLICATION = {"multiplication", "multi", "*"};
+    private final String[] DIVISION = {"division", "divide", "div", "/"};
     private final String[] QUIT = {"quit", "q"};
     private final String LINESTART = "> ";
 
