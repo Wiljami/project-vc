8d8703527b5658821aa8840e617caaa26be9334a
Viljami Pietarila <viljami.pietarila@gmail.com>
2019-10-01 12:24:35 +0300

    Delete StartConsoleUI.java
    With the ConsoleUI launch being the default launch mode for the app
    the StartConsoleUI.java is no longer used.

--- src/main/java/fi/tuni/tiko/StartConsoleUI.java
+++
@@ -1,8 +0,0 @@
-package fi.tuni.tiko;
-
-class StartConsoleUI {
-    public static void main(String[] args) {
-        ConsoleUI consoleUI = new ConsoleUI(new Calculator());
-        consoleUI.start();
-    }
-}
