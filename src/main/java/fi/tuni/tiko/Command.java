a3b9d6d3e1f0b07622de36b55efc3ef6454ac0c0
Viljami Pietarila <viljami.pietarila@gmail.com>
2019-10-02 18:34:26 +0300

    Command.java: Add getters and setters.
    Add getters and setters for desc and str in Command.java and implement
    the constructor to use them.

--- src/main/java/fi/tuni/tiko/Command.java
+++ src/main/java/fi/tuni/tiko/Command.java
@@ -4,7 +4,23 @@ class Command {
     private String[] str;
     private String desc;
     public Command(String[] str, String desc) {
-        this.str = str;
+        setStr(str);
+        setDesc(desc);
+    }
+
+    public String getDesc() {
+        return desc;
+    }
+
+    public void setDesc(String desc) {
         this.desc = desc;
+    }
+
+    public String[] getStr() {
+        return str;
+    }
+
+    public void setStr(String[] str) {
+        this.str = str;
     }
 }
