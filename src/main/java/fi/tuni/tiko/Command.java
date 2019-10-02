b137e59dc5e96cb6cd11c47db29d435ed6cd5f43
Viljami Pietarila <viljami.pietarila@gmail.com>
2019-10-02 18:33:00 +0300

    Command.java: Add constructor.
    Add constructor to Command.java. It now takes the String array of
    commands that the command needs and the description of the command.

--- src/main/java/fi/tuni/tiko/Command.java
+++ src/main/java/fi/tuni/tiko/Command.java
@@ -1,5 +1,10 @@
 package fi.tuni.tiko;
 
 class Command {
-
+    private String[] str;
+    private String desc;
+    public Command(String[] str, String desc) {
+        this.str = str;
+        this.desc = desc;
+    }
 }
