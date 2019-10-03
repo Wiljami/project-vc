f77091704c9e50cc5587344ce7bad31d58e2b325
Hanna Tuominen <hanna.h.tuominen@tuni.fi>
2019-10-03 09:14:31 +0300

    SnakePart:Add snakepart old & new ints and getset.
    Add snapePartRowsNew, snakePartCollsNew, snapePartRowsOld, snakePartCollsOld
    to SnakePart for getting the snakes current and old place when moving.
    Old place is used for the next part of the snake to move there.

--- src/main/java/fi/tuni/tiko/SnakePart.java
+++ src/main/java/fi/tuni/tiko/SnakePart.java
@@ -1,9 +1,70 @@
 package fi.tuni.tiko;
 
 class SnakePart {
+    private int snapePartRowsNew;
+    private int snakePartCollsNew;
+    private int snapePartRowsOld;
+    private int snakePartCollsOld;
 
-    public SnakePart() {
-
+    public SnakePart(int snapePartRowsNew, int snakePartCollsNew) {
+        setSnakePartCollsNew(snakePartCollsNew);
+        setSnapePartRowsNew(snapePartRowsNew);
+        setSnakePartCollsOld(snakePartCollsNew);
+        setSnapePartRowsOld(snapePartRowsNew);
     }
-    
+
+    /**
+     * @return the snakePartCollsNew
+     */
+    public int getSnakePartCollsNew() {
+        return snakePartCollsNew;
+    }
+    /**
+     * @param snakePartCollsNew the snakePartCollsNew to set
+     */
+    public void setSnakePartCollsNew(int snakePartCollsNew) {
+        this.snakePartCollsNew = snakePartCollsNew;
+    }
+
+    /**
+     * @return the snakePartCollsOld
+     */
+    public int getSnakePartCollsOld() {
+        return snakePartCollsOld;
+    }
+
+    /**
+     * @param snakePartCollsOld the snakePartCollsOld to set
+     */
+    public void setSnakePartCollsOld(int snakePartCollsOld) {
+        this.snakePartCollsOld = snakePartCollsOld;
+    }
+
+    /**
+     * @return the snapePartRowsNew
+     */
+    public int getSnapePartRowsNew() {
+        return snapePartRowsNew;
+    }
+
+    /**
+     * @param snapePartRowsNew the snapePartRowsNew to set
+     */
+    public void setSnapePartRowsNew(int snapePartRowsNew) {
+        this.snapePartRowsNew = snapePartRowsNew;
+    }
+
+    /**
+     * @return the snapePartRowsOld
+     */
+    public int getSnapePartRowsOld() {
+        return snapePartRowsOld;
+    }
+
+    /**
+     * @param snapePartRowsOld the snapePartRowsOld to set
+     */
+    public void setSnapePartRowsOld(int snapePartRowsOld) {
+        this.snapePartRowsOld = snapePartRowsOld;
+    }
 }
