6f14d43621504bee096c24e03664f0fb4d6a8fd6
Hanna Tuominen <hanna.h.tuominen@tuni.fi>
2019-10-08 09:01:19 +0300

    SnakeEnemyPart:Add getters and setters for the global attributes.
    Add get and set for each of the global int attributes, int id, snakeEnemyPartRowsNew, snakeEnemyPartCollsNew.

--- src/main/java/fi/tuni/tiko/SnakeEnemyPart.java
+++ src/main/java/fi/tuni/tiko/SnakeEnemyPart.java
@@ -4,8 +4,45 @@ class SnakeEnemyPart {
     int snakeEnemyPartRowsNew;
     int snakeEnemyPartCollsNew;
     int id;
-    
-    public SnakeEnemyPart(int snakeEnemyPartRowsNew, int snakeEnemyPartCollsNew, int id) {
 
+    public SnakeEnemyPart(int snakeEnemyPartRowsNew, int snakeEnemyPartCollsNew, int id) {
+        
+    }
+
+    /**
+     * @param snakeEnemyPartCollsNew the snakeEnemyPartCollsNew to set
+     */
+    public void setSnakeEnemyPartCollsNew(int snakeEnemyPartCollsNew) {
+        this.snakeEnemyPartCollsNew = snakeEnemyPartCollsNew;
+    }
+    /**
+     * @return the snakeEnemyPartCollsNew
+     */
+    public int getSnakeEnemyPartCollsNew() {
+        return snakeEnemyPartCollsNew;
+    }
+    /**
+     * @param snakeEnemyPartRowsNew the snakeEnemyPartRowsNew to set
+     */
+    public void setSnakeEnemyPartRowsNew(int snakeEnemyPartRowsNew) {
+        this.snakeEnemyPartRowsNew = snakeEnemyPartRowsNew;
+    }
+    /**
+     * @return the snakeEnemyPartRowsNew
+     */
+    public int getSnakeEnemyPartRowsNew() {
+        return snakeEnemyPartRowsNew;
+    }
+    /**
+     * @param id the id to set
+     */
+    public void setId(int id) {
+        this.id = id;
+    }
+    /**
+     * @return the id
+     */
+    public int getId() {
+        return id;
     }
 }
