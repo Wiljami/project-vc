a2f2944896c77fc7514234fefe8a77ef1b78750a
Maija <testi@testi.com>
2019-10-10 20:47:10 +0300

    Poem gen: Introduce generateBtn to PoemWindow.

--- src/main/java/fi/tuni/tiko/PoemWindow.java
+++ src/main/java/fi/tuni/tiko/PoemWindow.java
@@ -4,10 +4,13 @@ import java.awt.event.*;
 import java.awt.*;
 
 class PoemWindow extends JFrame{
+    JButton generateBtn;
 
     public PoemWindow(){
     setSize(300, 300);
     setTitle("Poem generator");
+
+    generateBtn = new JButton("Generate poem");
     }
     
 }
