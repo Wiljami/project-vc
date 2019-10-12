7ff6a909991ebe1133abfecbb0ad9c7a4b42f3ad
Maija <testi@testi.com>
2019-10-12 17:03:11 +0300

    Poem gen: Move GUI elements to createGUI().
    Move GUI elements to createGUI method in PoemWindow.

--- src/main/java/fi/tuni/tiko/PoemWindow.java
+++ src/main/java/fi/tuni/tiko/PoemWindow.java
@@ -11,20 +11,16 @@ class PoemWindow extends JFrame{
     public PoemWindow(){
     setSize(300, 300);
     setTitle("Poem generator");
-
-    setLayout(new BorderLayout());
-
-    generateBtn = new JButton("Generate poem");
-    poemTextArea = new JTextArea("test", 10, 10);
-
-    add(generateBtn);
-    add(poemTextArea);
-
     setVisible(true);
     }
 
     public void createGUI(){
+        setLayout(new BorderLayout());
+        generateBtn = new JButton("Generate poem");
+        poemTextArea = new JTextArea("test", 10, 10);
 
+        add(generateBtn);
+        add(poemTextArea);
     }
     
 }
