7d4aaa9c49f9dd4f5483d3baad62a34ddeeee40f
Viljami Pietarila <viljami.pietarila@gmail.com>
2019-10-19 20:59:25 +0300

    CalculatorConsole: Add command for graphical calc.
    Add Graphical calculator command to the CalculatorConsoleUI.

--- src/main/java/fi/tuni/tiko/CalculatorConsole.java
+++ src/main/java/fi/tuni/tiko/CalculatorConsole.java
@@ -2,42 +2,41 @@ package fi.tuni.tiko;
 
 import java.util.ArrayList;
 import java.util.List;
+
 /**
  * CalculatorConsole is a module for the console UI.
  * 
- * CalculatorConsole holds information and functions for Calculator App 
- * to be used with the ConsoleUI.
+ * CalculatorConsole holds information and functions for Calculator App to be
+ * used with the ConsoleUI.
  * 
  * @author Viljami Pietarila
  * @version 20191019
  */
 
 class CalculatorConsole {
-    private final String[] HELP = {"help", "h"};
+    private final String[] HELP = { "help", "h" };
     private final String HELPDESC = " - this printout.";
-    private final String[] ADDITION = {"addition", "add", "+"};
+    private final String[] ADDITION = { "addition", "add", "+" };
     private final String ADDITIONDESC = " - addition.";
-    private final String[] SUBSTRACTION = {"substraction", "sub", "-"};
+    private final String[] SUBSTRACTION = { "substraction", "sub", "-" };
     private final String SUBSTRACTIONDESC = "- substraction.";
-    private final String[] MULTIPLICATION = {"multiplication", "multi", "*"};
+    private final String[] MULTIPLICATION = { "multiplication", "multi", "*" };
     private final String MULTIPLICATIONDESC = " - multiplication.";
-    private final String[] DIVISION = {"division", "divide", "div", "/"};
+    private final String[] DIVISION = { "division", "divide", "div", "/" };
     private final String DIVISIONDESC = " - division.";
-    private final String[] FIBONACCI = {"fibonacci", "fibo", "f"};
+    private final String[] FIBONACCI = { "fibonacci", "fibo", "f" };
     private final String FIBONACCIDESC = " - fibonacci series";
-    private final String[] QUIT = {"quit", "q"};
+    private final String[] QUIT = { "quit", "q" };
     private final String QUITDESC = " - exits the program";
     final String BADCOMMAND = "Unknown command";
-    final String INTRO = 
-        "Welcome to the calculator console UI.\nType help for commands.";
+    final String INTRO = "Welcome to the calculator console UI.\nType help for commands.";
 
-    private final String[] GRAPHCALC = {"graphicalcalculator", "gcalc", "g"};
-    private final String[] CALCULATOR = {"calculator", "calc", "c"};
-    private final String[] POEM = {"poem", "p", "secret poem generator"};
-    private final String[] SNAKE = {"snake", "s", "snaek"};
+    private final String[] GRAPHCALC = { "graphicalcalculator", "gcalc", "g" };
+    private final String[] CALCULATOR = { "calculator", "calc", "c" };
+    private final String[] POEM = { "poem", "p", "secret poem generator" };
+    private final String[] SNAKE = { "snake", "s", "snaek" };
 
-    private final String WELCOMEMESSAGE = 
-        "Welcome to the Calculator!\nType help for commands";
+    private final String WELCOMEMESSAGE = "Welcome to the Calculator!\nType help for commands";
     private final String FIRSTVALUE = "Give first value";
     private final String SECONDVALUE = "Give second value";
     private final String SERIESTARGET = "How far into the series";
@@ -52,7 +51,8 @@ class CalculatorConsole {
      * CalculatorConsole constructor.
      * 
      * The constructor setups the commands.
-     * @param consoleUI Reference to the UI element
+     * 
+     * @param consoleUI  Reference to the UI element
      * @param calculator Reference to the App
      */
     public CalculatorConsole(ConsoleUI consoleUI, Calculator calculator) {
@@ -65,18 +65,23 @@ class CalculatorConsole {
     }
 
     private void setupBonusCommands() {
-        
+        commands.add(new Command(GRAPHCALC, "") {
+            @Override
+            void run() {
+                Window Window = new Window("calculator");
+            }
+        });
     }
 
     private void setupCommands() {
-        commands.add(new Command(HELP, HELPDESC){
+        commands.add(new Command(HELP, HELPDESC) {
             @Override
             void run() {
                 consoleUI.printHelp(commands);
             }
         });
 
-        commands.add(new Command(ADDITION, ADDITIONDESC){
+        commands.add(new Command(ADDITION, ADDITIONDESC) {
             @Override
             void run() {
                 int a = consoleUI.askValueWithPrompt(FIRSTVALUE);
@@ -85,7 +90,7 @@ class CalculatorConsole {
             }
         });
 
-        commands.add(new Command(SUBSTRACTION, SUBSTRACTIONDESC){
+        commands.add(new Command(SUBSTRACTION, SUBSTRACTIONDESC) {
             @Override
             void run() {
                 int a = consoleUI.askValueWithPrompt(FIRSTVALUE);
@@ -94,7 +99,7 @@ class CalculatorConsole {
             }
         });
 
-        commands.add(new Command(MULTIPLICATION, MULTIPLICATIONDESC){
+        commands.add(new Command(MULTIPLICATION, MULTIPLICATIONDESC) {
             @Override
             void run() {
                 int a = consoleUI.askValueWithPrompt(FIRSTVALUE);
@@ -103,7 +108,7 @@ class CalculatorConsole {
             }
         });
 
-        commands.add(new Command(DIVISION, DIVISIONDESC){
+        commands.add(new Command(DIVISION, DIVISIONDESC) {
             @Override
             void run() {
                 int a = consoleUI.askValueWithPrompt(FIRSTVALUE);
@@ -112,7 +117,7 @@ class CalculatorConsole {
             }
         });
 
-        commands.add(new Command(FIBONACCI, FIBONACCIDESC){
+        commands.add(new Command(FIBONACCI, FIBONACCIDESC) {
             @Override
             void run() {
                 int a = consoleUI.askValueWithPrompt(SERIESTARGET);
@@ -124,14 +129,14 @@ class CalculatorConsole {
             }
         });
 
-        commands.add(new Command(QUIT, QUITDESC){
+        commands.add(new Command(QUIT, QUITDESC) {
             @Override
             void run() {
-                consoleUI.quit(QUITMESSAGE);                
+                consoleUI.quit(QUITMESSAGE);
             }
         });
     }
-    
+
     public List<Command> getCommands() {
         return commands;
     }
