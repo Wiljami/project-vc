88129a11e4c6f7bdcf1c67ec5d24551f1e4b99c1
Hanna Tuominen <hanna.h.tuominen@tuni.fi>
2019-10-21 17:52:43 +0300

    SnakeUI: Make SnakeUI constructor generate window.
    Make SnakeUI constructor generate the initial window with size, title and location.

--- src/main/java/fi/tuni/tiko/SnakeUI.java
+++ src/main/java/fi/tuni/tiko/SnakeUI.java
@@ -4,8 +4,15 @@ import javax.swing.JFrame;
 
 public class SnakeUI extends JFrame {
 
+    SecretSnake snake;
+
     public SnakeUI() {
- 
+        snake = new SecretSnake();
+        setTitle("Snake");
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setLocation(100, 100);
+        setSize(500, 400);
+        setVisible(true);
     }
 
     public static void main(String []args) {
