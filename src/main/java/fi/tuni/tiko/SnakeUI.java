bc7848b3886378040083a78a6f431f0003d7ac00
Hanna Tuominen <hanna.h.tuominen@tuni.fi>
2019-10-21 17:51:47 +0300

    SnakeUI: Add main class for testing purposes.
    Add main class for testing the UI class.

--- src/main/java/fi/tuni/tiko/SnakeUI.java
+++ src/main/java/fi/tuni/tiko/SnakeUI.java
@@ -5,6 +5,11 @@ import javax.swing.JFrame;
 public class SnakeUI extends JFrame {
 
     public SnakeUI() {
-        
+ 
+    }
+
+    public static void main(String []args) {
+        SnakeUI ui = new SnakeUI();
     }
 }
+
