7fee0fd76ed3dc4512e6fb47a1d5ae5ea979424e
Hanna Tuominen <hanna.h.tuominen@tuni.fi>
2019-10-22 09:44:40 +0300

    CoffeeCounter: Update askAboutCoffee to work.
    Update the askAboutCoffee method to work so it asks the user how much coffee they have drank that day and gives a go for more or tells them to stop depending on the amount the give.

--- src/main/java/fi/tuni/tiko/CoffeeCounter.java
+++ src/main/java/fi/tuni/tiko/CoffeeCounter.java
@@ -1,13 +1,24 @@
 package fi.tuni.tiko;
 
+import java.util.Scanner;
+
 public class CoffeeCounter {
+    int enoughCoffee = 10;
+    int tooMuchCoffee = 15;
 
     public CoffeeCounter() {
         askAboutCoffee();
     }
 
     public void askAboutCoffee() {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("How many cups of coffee have you drunk so far today?");
+        int amount = Integer.parseInt(null, scan.nextInt());
 
+        if(amount <= enoughCoffee) {
+            System.out.println("Not enough coffee, you can drink more!");
+        } else if (amount >= tooMuchCoffee) {
+            System.out.println("Enough coffee, you can drink more, but maaaaaayyybe you should not, your choice tho!");
+        }
     }
-
 }
